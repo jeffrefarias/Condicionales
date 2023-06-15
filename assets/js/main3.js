60135724c69ee93d6d8fd019c94e535e31714dd0
Jeffrey Farías <jeffreyfarias5@gmail.com>
2023-06-15 00:16:02 -0400

    guardo variables dentro de function

--- assets/js/main3.js
+++ assets/js/main3.js
@@ -1,11 +1,12 @@
-//EJERCICIO 3
+
+
+
+  btnValidar.addEventListener("click", function(){
+    //EJERCICIO 3
 
   const num1 = document.querySelector("#num1").value;
   const num2 = document.querySelector("#num2").value;
   const num3 = document.querySelector("#num3").value;
-
-  btnValidar.addEventListener("click", function(){
-    console.log("Boton");
 
      if (num1 === "9" && num2 === "1" && num3 === "1") {
         document.querySelector("#alerta").innerHTML = "PASSWORD CORRECTO";
